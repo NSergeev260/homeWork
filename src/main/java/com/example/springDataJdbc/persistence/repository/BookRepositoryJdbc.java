aa6e8d92283603f4ef3debe6008f0edcf4007d29
nik <nikita.sergeev86@gmail.com>
2025-11-09 14:38:42 +0300

    change logic of BookRepositoryJdbc (the app uses NamedParameterJdbcTemplate now)

--- src/main/java/com/example/springDataJdbc/persistence/repository/BookRepositoryJdbc.java
+++ src/main/java/com/example/springDataJdbc/persistence/repository/BookRepositoryJdbc.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,34 +19,56 @@ import java.util.UUID;
 @AllArgsConstructor
 public class BookRepositoryJdbc implements BookRepository {
 
-    private static final String INSERT_BOOK = "INSERT INTO books(id, title, author, publication_year) VALUES (?, ?, ?, ?)";
-    private static final String GET_BOOK_BY_ID = "SELECT * FROM books WHERE id = ?";
-    private static final String GET_BOOK_BY_TITLE = "SELECT * FROM books WHERE title = ?";
-    private static final String GET_ALL_BOOKS = "SELECT * FROM books";
-    private static final String UPDATE_BOOK = "UPDATE books SET title = ?, author = ?, publication_year = ? WHERE id = ?";
-    private static final String DELETE_BOOK = "DELETE FROM books WHERE id = ?";
+    private static final String INSERT_BOOK =
+            "INSERT INTO books(id, title, author, publication_year) " +
+                    "VALUES (:id, :title, :author, :publicationYear)";
 
-    private final JdbcTemplate jdbcTemplate;
+    private static final String GET_BOOK_BY_ID =
+            "SELECT * FROM books WHERE id = :id";
+
+    private static final String GET_BOOK_BY_TITLE =
+            "SELECT * FROM books WHERE title = :title";
+
+    private static final String GET_ALL_BOOKS =
+            "SELECT * FROM books";
+
+    private static final String UPDATE_BOOK =
+            "UPDATE books SET title = :title, author = :author, " +
+                    "publication_year = :publicationYear WHERE id = :id";
+
+    private static final String DELETE_BOOK =
+            "DELETE FROM books WHERE id = :id";
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final BookRowMapper bookRowMapper;
 
     @Override
     public BookData insertBook(BookData book) {
         UUID newId = UUID.randomUUID();
-        jdbcTemplate.update(
-                INSERT_BOOK, newId, book.getTitle(),
-                book.getAuthor(), book.getPublicationYear());
+        SqlParameterSource insertParameters = new MapSqlParameterSource()
+                .addValue("id", newId)
+                .addValue("title", book.getTitle())
+                .addValue("author", book.getAuthor())
+                .addValue("publicationYear", book.getPublicationYear());
+        namedParameterJdbcTemplate.update(INSERT_BOOK, insertParameters);
         book.setId(newId);
+
         return book;
     }
 
     @Override
     public Optional<BookData> findBookById(UUID id) {
         try {
-            BookData bookData = jdbcTemplate.queryForObject(
-                    GET_BOOK_BY_ID, bookRowMapper, id);
+            SqlParameterSource findParameters = new MapSqlParameterSource()
+                    .addValue("id", id);
+            BookData bookData = namedParameterJdbcTemplate.queryForObject(
+                    GET_BOOK_BY_ID, findParameters, bookRowMapper);
+
             return Optional.ofNullable(bookData);
+
         } catch (EmptyResultDataAccessException e) {
             log.debug("Book not found. FAIL! id: {}", id);
+
             return Optional.empty();
         }
     }
@@ -51,29 +76,43 @@ public class BookRepositoryJdbc implements BookRepository {
     @Override
     public Optional<BookData> findBookByTitle(String title) {
         try {
-            BookData bookData = jdbcTemplate.queryForObject(
-                    GET_BOOK_BY_TITLE, bookRowMapper, title);
+            SqlParameterSource findParameters = new MapSqlParameterSource()
+                    .addValue("title", title);
+            BookData bookData = namedParameterJdbcTemplate.queryForObject(
+                    GET_BOOK_BY_TITLE, findParameters, bookRowMapper);
+
             return Optional.ofNullable(bookData);
+
         } catch (EmptyResultDataAccessException e) {
             log.debug("Book not found. FAIL! title: {}", title);
+
             return Optional.empty();
         }
     }
 
     @Override
     public List<BookData> findAllBooks() {
-        return jdbcTemplate.query(GET_ALL_BOOKS, bookRowMapper);
+
+        return namedParameterJdbcTemplate.query(
+                GET_ALL_BOOKS, new MapSqlParameterSource(), bookRowMapper);
     }
 
     @Override
     public BookData updateBook(BookData book) {
-        jdbcTemplate.update(UPDATE_BOOK, book.getTitle(),
-                book.getAuthor(), book.getPublicationYear(), book.getId());
+        SqlParameterSource updateParameters = new MapSqlParameterSource()
+                .addValue("id", book.getId())
+                .addValue("title", book.getTitle())
+                .addValue("author", book.getAuthor())
+                .addValue("publicationYear", book.getPublicationYear());
+        namedParameterJdbcTemplate.update(UPDATE_BOOK, updateParameters);
+
         return book;
     }
 
     @Override
     public void deleteBookById(UUID id) {
-        jdbcTemplate.update(DELETE_BOOK, id);
+        SqlParameterSource deleteParameters = new MapSqlParameterSource()
+                .addValue("id", id);
+        namedParameterJdbcTemplate.update(DELETE_BOOK, deleteParameters);
     }
 }
